e1fa03e628768fa55a35be1af5f3c1644045727d
Nishat Hoque <hoquen0397@gmail.com>
2021-02-20 15:24:33 -0500

    SearchBar

---
+++ src/SearchBar/SearchBar.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+import './SearchBar.css'
+
+class SearchBar extends Component {
+    render() { 
+        return (  
+            <div className = 'SearchBar'>
+                <div className = 'SearchBarHeading'>
+                    <h1> Google Book Search </h1>
+                </div>
+                <div className = 'SearchBarControls'>
+    
+                </div>
+            </div>
+        )
+    }
+}
+ 
+export default SearchBar;
